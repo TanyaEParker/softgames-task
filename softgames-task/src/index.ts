d19b1917acdf7c43096bbefb96e117ea78756ea1
TanyaEParker <tanyaparkerdev@gmail.com>
2025-04-14 13:12:41 +0100

    Manager setup

--- softgames-task/src/index.ts
+++ softgames-task/src/index.ts
@@ -1,6 +1,66 @@
-import { Application } from 'pixi.js';
+import {Application, Text} from 'pixi.js';
+import { manager } from './utilities/manager';
 
 const app = new Application();
 
-await app.init({ background: '#1099bb', resizeTo: window });
-document.body.appendChild(app.canvas);
+async function setup()
+{
+    await app.init(
+    { 
+      background: '#6b078c', 
+      resizeTo: window,
+    });
+
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(app.canvas);
+};
+
+(async () =>
+{
+	await setup();
+	await new MainMenu(app);
+})();
+
+class MainMenu
+{
+	private app : Application;
+	private manager : manager;
+	private FPS : Text;
+
+	constructor(app:Application)
+	{
+		this.app = app;
+		this.FPS = new Text({text: 'FPS: 0', style:{ fontSize: 18, fill: 0x000000 }});
+	    this.FPS.x = 10;
+	    this.FPS.y = 10;
+
+	    this.app.stage.addChild(this.FPS);
+	    this.app.ticker.add(() => this.updateFPS());
+
+	    this.manager = new manager(this.app,this.FPS)
+	}
+
+	private updateFPS():void
+	{
+		const frame = this.app.ticker.FPS.toFixed(0);
+		this.FPS.text = `FPS: ${frame}`
+	}
+
+
+	private setScene(task:string):void
+	{
+		switch(task)
+		{
+		case 'AoC':
+			this.sceneManager.loadScene(new AoC(this.app))
+			break;
+		case 'MW':
+			this.sceneManager.loadScene(new MW(this.app))
+			break;
+		case 'PF':
+			this.sceneManager.loadScene(new PF(this.app))
+			break;
+		}
+	}
+};
---
+++ softgames-task/src/utilities/manager.ts
@@ -0,0 +1,35 @@
+import { Application, Text } from 'pixi.js';
+ 
+export class manager
+{
+  private app: Application;
+  private currentScene: any;
+  private FPS : Text;
+
+  constructor(app: Application, FPS: Text)
+  {
+    this.app = app;
+    this.FPS = FPS;
+  }
+
+  public changeScene(newScene: any): void
+  {
+    if (this.currentScene)
+    {
+      this.app.stage.children.forEach((child) =>
+      {
+        if (!(child instanceof Text && child === this.FPS))
+        {
+          this.app.stage.removeChild(child);
+        }
+      });
+
+      if(this.currentScene.destroy)
+      {
+        this.currentScene.destroy();
+      }
+    }
+    this.currentScene = newScene;
+    this.currentScene.init();
+  }
+}
