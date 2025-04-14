60e0887a2ed9686954f9e56de097a8a98201df9e
TanyaEParker <tanyaparkerdev@gmail.com>
2025-04-14 12:47:51 +0100

    pixi impl

--- softgames-task/src/index.ts
+++ softgames-task/src/index.ts
@@ -1,3 +1,6 @@
-import {application} from 'pixi.js'
+import { Application } from 'pixi.js';
 
 const app = new Application();
+
+await app.init({ background: '#1099bb', resizeTo: window });
+document.body.appendChild(app.canvas);
